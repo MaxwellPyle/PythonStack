bb7c960d5b5f08cdad7b704a099daf38a615abf2
Maxwell Pyle <maxwell.r.pyle@gmail.com>
2022-01-18 16:46:22 -0500

    commit

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, redirect, session
 app = Flask(__name__)
 app.secret_key = "safetydance"
 
 @app.route('/')
 def index():
+    if "counter" not in session:
+        session["counter"] = 0
     return render_template("index.html")
 
+@app.route('/increment')
+def increment():
+    session["counter"] += 1
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+    
 if __name__ == "__main__":
     app.run(debug=True)
