0967901c89ee661871c50522dac4c6740a0e0ffa
Maxwell Pyle <maxwell.r.pyle@gmail.com>
2022-01-19 14:35:27 -0500

    new commit

--- flask/fundamentals/DojoSurvey/server.py
+++ flask/fundamentals/DojoSurvey/server.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "safetydance"
@@ -5,6 +6,18 @@ app.secret_key = "safetydance"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['favlang'] = request.form['favlang']
+    session['comments'] = request.form['comments']
+    return redirect('/success')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
